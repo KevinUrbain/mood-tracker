d4102349bb5613e0033e4ea0c7f220047a456feb
Kevin <k.urbain.personnel@gmail.com>
2026-02-24 11:41:39 +0100

    Change visibility of isConnected and redirectIfNotConnected methods to protected

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -18,7 +18,7 @@ abstract class Controller
         require_once dirname(__DIR__, 2) . '/views/templates/layout.php';
     }
 
-    public function isConnected(): bool
+    protected function isConnected(): bool
     {
         if (!empty($_SESSION) && isset($_SESSION['user'])) {
             return true;
@@ -26,7 +26,7 @@ abstract class Controller
         return false;
     }
 
-    public function redirectIfNotConnected(): void
+    protected function redirectIfNotConnected(): void
     {
         if (!$this->isConnected()) {
             header('Location: ' . PROJECT_URL . 'login');
